d1f2b7bdf434cb0fbcb34d6a00b1d5716ddc882a
Angelo Mahrouchi <angelo.mahrouchi@redlab.io>
2020-03-05 16:44:43 +0100

    CLI test script

---
+++ app/script.cli.php
@@ -0,0 +1,6 @@
+<?php
+
+echo __LINE__ . ': This is just a test script...' . PHP_EOL;
+echo __LINE__ . ': This is just a test script...' . PHP_EOL;
+echo __LINE__ . ': This is just a test script...' . PHP_EOL;
+echo __LINE__ . ': This is just a test script...' . PHP_EOL;
